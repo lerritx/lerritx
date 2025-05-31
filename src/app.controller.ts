cda9d2a6a263bb8a17c46cf8ddff5496ba1b4aee
lerritx <daf123456ch@gmail.com>
2025-05-31 14:22:35 +0700

    it2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,9 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/it')
-  getit(): string{
-    return 'it works';
+  @Get('/it2')
+  getit2(): string {
+    return 'it works2';
   }
-
 }
